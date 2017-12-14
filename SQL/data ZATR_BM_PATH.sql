3a007dd6e3f7b1505c3d27c56c237ab2620e2abf
Ihar Sevizdral <kasperq@mail.ru>
2017-12-14 16:55:32 +0300

    added SQL folder

---
+++ SQL/data ZATR_BM_PATH.sql
@@ -0,0 +1,32 @@
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('0100', 'f:\bmomts\bm1\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('0400', 'f:\bmomts\bm1\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('2412', 'f:\bmomts\bm1\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('0200', 'f:\bmomts\bm1\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('0300', 'f:\bmomts\bm3\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('0600', 'f:\bmomts\bm3\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('1200', 'f:\bmomts\bm3\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('0500', 'f:\bmomts\bm4\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('0800', 'f:\bmomts\bm4\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('1200', 'f:\bmomts\bm3\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('2100', 'f:\bmomts\bm4\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('0700', 'f:\bmomts\bm4\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('1600', 'f:\bmomts\bm8\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('1700', 'f:\bmomts\bm8\zerno1     ');
+INSERT INTO ZATR_BM_PATH (STKOD, PATH)
+                  VALUES ('1800', 'f:\bmomts\bm8\zerno1     ');
+
+COMMIT WORK;
